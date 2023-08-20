7922aa6988994cf2b5f4344be3da764406f608c4
wenh <62581720+bert-wen@users.noreply.github.com>
2023-08-20 12:51:11 +0800

    初始化项目

---
+++ chatbot-interfaces/src/main/java/com/sveinn/chatbotinterfaces/ChatBotInterfacesApplication.java
@@ -0,0 +1,20 @@
+package com.sveinn.chatbotinterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: chatbot
+ * @package: com.sveinn.chatbotinterfaces
+ * @className: ChatBotInterfacesApplication
+ * @author: WenHui
+ * @description: 启动入口
+ * @date: 2023/8/20 12:07
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ChatBotInterfacesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotInterfacesApplication.class,args);
+    }
+}
